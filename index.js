9af8967a897828d154aa0b063b5dd221eeaf85f8
T-amairi <amairitar@gmail.com>
2023-07-28 22:36:16 -0400

    basic auth

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const basicAuth = require('express-basic-auth')
+require("dotenv").config();
 
-const app = express()
-const port = 3000
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const basicAuth = require("express-basic-auth");
+const morgan = require('morgan')
 
-// app.use(basicAuth({
-//     users: { 'admin': 'admin' }
-// })) 
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(helmet())
+app.use(
+  basicAuth({
+    users: {
+      [process.env.BASIC_AUTH_USERNAME]: process.env.BASIC_AUTH_PASSWORD,
+    },
+    challenge: true,
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(morgan('combined'))
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
